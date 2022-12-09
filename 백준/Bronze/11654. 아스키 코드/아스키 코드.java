ce00e443b6b5ca12501a7454268b3b18110f1368
lkdcode <110602069+lkdcode@users.noreply.github.com>
2022-12-10 00:40:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner SCAN = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        char input = SCAN.next().charAt(0);
+
+        System.out.println(input - 0);
+    }
+}
