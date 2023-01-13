c3cbf2f4c06223179685da006c2b24becc6ccf9c
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-01-13 10:35:18 +0900

    [Silver IV] Title: 제로, Time: 244 ms, Memory: 23524 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int size = Integer.parseInt(br.readLine());
+
+        Stack<Integer> numbers = new Stack<>();
+        for (int i = 0; i < size; i++) {
+            int inputNumber = Integer.parseInt(br.readLine());
+            if (inputNumber != 0) {
+                numbers.push(inputNumber);
+            } else {
+                numbers.pop();
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            result += numbers.get(i);
+        }
+        System.out.println(result);
+    }
+}
