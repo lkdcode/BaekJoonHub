b59fb32ffe937d7a669fda9dc62c0c2ef49b6c87
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-01-17 15:42:29 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,16 +1,36 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner scan= new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        
-        if(a>b){
-            System.out.printf(">");
-        }else if (a<b){
-            System.out.printf("<");
-        }
-        else
-            System.out.printf("==");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+interface Result {
+    public String make(int a, int b);
+}
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        Result result = new Result() {
+            @Override
+            public String make(int a, int b) {
+                if (a > b) {
+                    return ">";
+                } else if (a < b) {
+                    return "<";
+                } else {
+                    return "==";
+                }
+            }
+
+
+        };
+
+        String input = br.readLine();
+        String[] inputSplit = input.split(" ");
+        int firstNumber = Integer.parseInt(inputSplit[0]);
+        int secondNumber = Integer.parseInt(inputSplit[1]);
+
+        System.out.println(result.make(firstNumber, secondNumber));
+
     }
 }
