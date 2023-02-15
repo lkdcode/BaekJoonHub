080f10c90c84228d4fa6f66e12fbfcf481fe8251
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-02-15 22:29:26 +0900

    [Bronze I] Title: 소인수분해, Time: 244 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int number = scanner.nextInt();
+
+        int n = 2;
+
+        while (number != 1) {
+
+            if (number % n == 0) {
+                System.out.println(n);
+                number/=n;
+            } else {
+                n++;
+            }
+        }
+
+    }
+}
