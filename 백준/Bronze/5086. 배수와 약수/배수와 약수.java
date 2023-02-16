f48188799e5a2e11478009d5131486483b517182
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-02-16 17:27:36 +0900

    [Bronze III] Title: 배수와 약수, Time: 200 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+
+
+        while (firstNumber != 0 && secondNumber != 0) {
+
+            if (secondNumber % firstNumber == 0) {
+                System.out.println("factor");
+            } else if (firstNumber % secondNumber == 0) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+
+            firstNumber = scanner.nextInt();
+            secondNumber = scanner.nextInt();
+        }
+    }
+}
