3e88be8a0342dc1f613b4f502c718a76f013d462
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-02-23 17:18:47 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 144 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        long number = Long.parseLong(br.readLine());
+        System.out.println((number * (number - 1) * (number - 2)) / 6 + "\n3");
+    }
+}
