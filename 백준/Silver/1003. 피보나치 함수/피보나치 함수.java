47adefd54e3620abc80566c148a79b24c838a033
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-02-28 16:48:53 +0900

    [Silver III] Title: 피보나치 함수, Time: 152 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -5,14 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int[] fibonacci = new int[41];
-    private static int resultZero = 0;
-    private static int resultOne = 0;
 
     public static void main(String[] args) throws IOException {
         int size = Integer.parseInt(br.readLine());
         makeFibonacci();
+        
         for (int i = 0; i < size; i++) {
             int number = Integer.parseInt(br.readLine());
+
             if (number == 0) {
                 System.out.println("1 0");
                 continue;
@@ -24,24 +24,6 @@ public class Main {
         }
 
     }
-
-    //     0 1 1 2 3 5 8 13 21
-    //0 -> 1 0 1 1 2 3 5
-    //1 -> 0 1 1 2 3 5 8 13 21 34
-
-
-//    static int fibonacci(int n) {
-//        if (n == 0) {
-//            resultZero++;
-//            return 0;
-//        } else if (n == 1) {
-//            resultOne++;
-//            return 1;
-//        } else {
-//            return fibonacci(n - 1) + fibonacci(n - 2);
-//        }
-//    }
-
 
     static void makeFibonacci() {
         fibonacci[0] = 0;
