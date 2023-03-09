9484fc70370e32480fc44cd42e763a7a4871d91a
JAva_BE <110602069+lkdcode@users.noreply.github.com>
2023-03-09 14:00:43 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18244 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int number = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[number + 1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= number; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+                dp[i] = dp[i / 2] + 1;
+            }
+
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+                dp[i] = dp[i / 3] + 1;
+            }
+        }
+
+        System.out.println(dp[number]);
+
+    }
+}
