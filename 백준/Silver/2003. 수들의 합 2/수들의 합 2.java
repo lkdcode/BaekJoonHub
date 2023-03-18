816706d3471efdecc298ed4ece3c223b522707e9
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-03-19 03:30:30 +0900

    [Silver IV] Title: 수들의 합 2, Time: 212 ms, Memory: 16840 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int size = Integer.parseInt(st.nextToken());
+        int findNumber = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < size; i++) {
+            int sum = numbers[i];
+
+            for (int j = i + 1; j < size; j++) {
+                if (sum == findNumber) {
+                    result++;
+                    sum = -1;
+                    break;
+                } else if (sum > findNumber) {
+                    break;
+                }
+                sum += numbers[j];
+            }
+            if (sum == findNumber) {
+                result++;
+            }
+
+        }
+
+        System.out.println(result);
+
+
+    }
+}
