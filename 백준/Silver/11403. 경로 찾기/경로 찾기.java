4e37d8006cc1b2f97f291374f16db10becce208b
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-04-17 17:36:24 +0900

    [Silver I] Title: 경로 찾기, Time: 388 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int size = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < size; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (map[i][k] == 1 && map[k][j] == 1) {
+                        map[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
