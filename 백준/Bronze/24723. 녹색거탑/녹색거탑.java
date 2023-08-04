93fe22fdd5bf44a7235d3c136d0f5f920618e673
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-08-04 11:33:36 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((int)Math.pow(2, n));
+
+    }
+
+}
