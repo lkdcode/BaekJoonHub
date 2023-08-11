314baf08a1ad644b8b7eaa972d7458e7786f86fb
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-08-11 12:38:23 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(factorial(Long.parseLong(br.readLine())));
+    }
+
+    private static long factorial(long number) {
+        if (number == 1 || number == 0) {
+            return 1;
+        }
+        return factorial(number - 1) * number;
+    }
+
+}
