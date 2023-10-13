309f7be36cd92954af65ab5d120a34c974aa25ce
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-10-13 21:36:49 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.62 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        
+        return num_list.length > 10
+            ? Arrays.stream(num_list).sum()
+            : Arrays.stream(num_list).reduce(1, (a, b) -> a * b)
+                ;
+    }
+}
