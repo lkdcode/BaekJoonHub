1a0cc545deab473d218fdf5ce5a0dcdc32ff486a
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-10-16 15:44:09 +0900

    [level unrated] Title: l로 만들기, Time: 14.42 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,16 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        
-        for (int i = 0; i < myString.length(); i++) {
-            char check = myString.charAt(i);
-            if (check - 0 < 'l' - 0) {
-                answer += "l";
-            } else {
-                answer += String.valueOf(check);
-            }
-        }
-        
-        return answer;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
