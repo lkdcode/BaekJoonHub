027bf5864500eef3ba478039c2cf5c16541bb3c8
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-11-01 00:57:26 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.11 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int index = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    answer[i][j] = 1;
+                }
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
