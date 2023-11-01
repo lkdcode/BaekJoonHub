705d99cab5a82f4947ee2d0724721af39a61d9ec
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-11-01 11:31:30 +0900

    [level unrated] Title: 세 개의 구분자, Time: 90.35 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,30 +3,17 @@ import java.util.*;
 class Solution {
     public String[] solution(String myStr) {
         String[] answer = {"EMPTY"};
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < myStr.length(); i++) {
-            String check = String.valueOf(myStr.charAt(i));
-            
-            if (check.equals("a") || check.equals("b") || check.equals("c")) {
-                if (sb.length() != 0) {
-                list.add(sb.toString());
-                }
-                sb = new StringBuilder();
-                continue;
-            }
-            sb.append(check);
-        }
+        String[] answer1 = Arrays
+            .stream(myStr.split("[abc]"))
+            .filter(e -> !e.isEmpty())
+            .toArray(String[]::new);
         
-        if (sb.length() != 0) {
-            list.add(sb.toString());
-        }
-        
-        // System.out.println(list);
-        
-        if (list.size() == 0) return answer;
-        
-        return list.toArray(String[]::new);
+        return Arrays.stream(myStr.split("[abc]"))
+            .filter(e -> !e.isEmpty())
+            .toArray(String[]::new)
+            .length == 0 ? new String[]{"EMPTY"} : Arrays.stream(myStr.split("[abc]"))
+            .filter(e -> !e.isEmpty())
+            .toArray(String[]::new);
     }
 }
