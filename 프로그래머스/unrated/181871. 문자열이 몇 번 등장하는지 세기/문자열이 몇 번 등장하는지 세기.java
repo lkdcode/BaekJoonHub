eac202987b13b0851ebad3a22efe8ed798d0f7f9
lkdcode <110602069+lkdcode@users.noreply.github.com>
2023-11-01 10:49:58 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.08 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.substring(i, myString.length()).startsWith(pat)) answer++;
+        }
+        
+        return answer;
+    }
+}
